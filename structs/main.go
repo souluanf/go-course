c85a52fce35a1656ed19924fdd0ce5c7da7e1cfe
Luan Fernandes <souluanf@icloud.com>
2023-03-03 00:24:06 -0300

    herança com structs

--- structs/main.go
+++ structs/main.go
@@ -8,6 +8,11 @@ type Car struct {
 	Color string
 }
 
+type SuperCar struct {
+	Car
+	CanFly bool
+}
+
 func (c Car) toString() string {
 	return fmt.Sprintf("{\"name\": \"%s\", \"year\": %d, \"color\": \"%s\"}", c.Name, c.Year, c.Color)
 }
@@ -15,10 +20,9 @@ func (c Car) toString() string {
 func main() {
 	car1 := Car{"X1", 2016, "Black"}
 	car2 := Car{"Corolla", 2015, "Black"}
-
-	fmt.Println(car1.Name, car1.Year, car1.Color)
-	fmt.Println(car2.Name, car2.Year, car2.Color)
+	scar := SuperCar{Car: Car{"Ferrari", 2030, "Red"}, CanFly: false}
 
 	fmt.Println(car1.toString())
 	fmt.Println(car2.toString())
+	fmt.Println(scar.toString())
 }
