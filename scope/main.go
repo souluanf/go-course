e610042132be8e37393942d12038451300049c58
Luan Fernandes <souluanf@icloud.com>
2023-03-02 21:47:01 -0300

    scope

---
+++ scope/main.go
@@ -0,0 +1,17 @@
+package scope
+
+import "fmt"
+
+var y int = 20
+
+//functions main() {
+//	x := 10
+//	fmt.Println(x)
+//	fmt.Println(y)
+//	fmt.Println(z)
+//	printZ()
+//}
+
+func PrintY() {
+	fmt.Println(y)
+}
---
+++ scope/other.go
@@ -0,0 +1,7 @@
+package scope
+
+var z string = "Hello Other"
+
+func PrintZ() {
+	println(z)
+}
