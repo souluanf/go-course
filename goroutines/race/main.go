0be7b1e7c718303311735fc413461302b554c94d
Luan Fernandes <souluanf@icloud.com>
2023-03-03 22:31:11 -0300

    Mutex

--- goroutines/race/main.go
+++ goroutines/race/main.go
@@ -3,27 +3,31 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 var result int
+var m sync.Mutex // Sempre que precisar proteger dados de mudança de valores
 
 func main() {
 	go runProcess("P1", 20)
 	go runProcess("P2", 20)
 	var s string
-	fmt.Scanln(&s)
+	_, _ = fmt.Scanln(&s)
 	fmt.Println("Final result: ", result)
 }
 
 func runProcess(name string, total int) {
 	for i := 0; i < total; i++ {
-		z := result
-		z++
 		t := time.Duration(rand.Intn(255))
 		time.Sleep(time.Millisecond * t)
-		result = z
+		m.Lock()
+		result++
 		fmt.Println(name, "->", i, " Partial result: ", result)
+		m.Unlock()
 	}
 
 }
+
+// go run -race main.go
