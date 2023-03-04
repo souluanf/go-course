3da8a594530e2acfbe17eddf81e0fe81dd45aa78
Luan Fernandes <souluanf@icloud.com>
2023-03-04 12:05:50 -0300

    Renderizando templates

--- web/main.go
+++ web/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func main() {
+type Post struct {
+	Id    int
+	Title string
+	Body  string
+}
 
+func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello world")
+		t := template.Must(template.ParseFiles("templates/index.htm"))
+		if err := t.ExecuteTemplate(writer, "index.html", nil); err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
 	})
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
