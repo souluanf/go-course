c4ba70818f2d02053d8fa606840fef4dd8efd0c7
Luan Fernandes <souluanf@icloud.com>
2023-03-05 13:21:55 -0300

    Trabalhando com QueryRow

--- web/main.go
+++ web/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"database/sql"
-	_ "database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	_ "github.com/gorilla/mux"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type Post struct {
@@ -21,7 +20,12 @@ var db, _ = sql.Open("mysql", "go_course:go_course@/go_course")
 
 func main() {
 	r := mux.NewRouter()
+
+	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static/"))))
+
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/{id}/view", ViewHandler)
+
 	fmt.Println(http.ListenAndServe(":8080", r))
 }
 
@@ -30,6 +34,21 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if err := t.ExecuteTemplate(w, "index.html", ListPosts()); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	t := template.Must(template.ParseFiles("templates/view.html"))
+	if err := t.ExecuteTemplate(w, "view.html", GetPostById(id)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func GetPostById(id int) Post {
+	row := db.QueryRow("select * from posts where id = ?", id)
+	post := Post{}
+	row.Scan(&post.Id, &post.Title, &post.Body)
+	return post
 }
 
 func ListPosts() []Post {
