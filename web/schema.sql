bc49e77795c8e9a63baf68e400e6345f6709ed83
Luan Fernandes <souluanf@icloud.com>
2023-03-05 12:45:23 -0300

    schema.sql

--- web/schema.sql
+++ web/schema.sql
@@ -3,10 +3,24 @@ CREATE USER 'go_course'@'localhost' IDENTIFIED WITH mysql_native_password BY 'go
 GRANT ALL PRIVILEGES ON go_course.* TO 'go_course'@'localhost';
 FLUSH PRIVILEGES;
 
-
-create table go_course.posts
+create table posts
 (
-    id    int auto_increment primary key,
-    title varchar(255) null,
-    body  text         null
-);
+    id         int not null auto_increment,
+    title      varchar(255),
+    body       text,
+    primary key (id)
+) engine = InnoDB;
+
+
+insert into posts (title, body)
+values
+    ('Post 1', 'Post 1 body'),
+    ('Post 2', 'Post 2 body'),
+    ('Post 3', 'Post 3 body'),
+    ('Post 4', 'Post 4 body'),
+    ('Post 5', 'Post 5 body'),
+    ('Post 6', 'Post 6 body'),
+    ('Post 7', 'Post 7 body'),
+    ('Post 8', 'Post 1 body'),
+    ('Post 9', 'Post 9 body'),
+    ('Post 10', 'Post 10 body');
